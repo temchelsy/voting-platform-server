6547f1303120769114ee0c33e5ea5d84cb3c282b
temchelsy <148960005+temchelsy@users.noreply.github.com>
2025-01-16 07:28:46 +0100

    modified content

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(path.resolve(), 'public')));
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://electrify-chi.vercel.app/', 
   methods: ['GET', 'POST', "DELETE", "PUT", "PATCH"],
   credentials: true
 }));
